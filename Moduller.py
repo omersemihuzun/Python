288f14a0183a0654c59b92135175be78afa3b514
Ömer Semih <omeruzunsemih@gmail.com>
2025-07-22 21:44:54 +0300

    moduller

--- Moduller.py
+++ Moduller.py
@@ -1,8 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as matplot
 
-
-maasListesi = np.random.normal(4000,500,1000)
+maasListesi = np.random.normal(4000,10,300)
 print(np.mean(maasListesi))
 
 matplot.hist(maasListesi,50)
